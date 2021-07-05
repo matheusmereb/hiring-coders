856460fc224fc00e69446cca12148d6b897fb5ef
Matheus Mereb <matheus.mereb@gmail.com>
2021-07-05 16:05:39 -0300

    Added some reference

--- 04-javascript/script.js
+++ 04-javascript/script.js
@@ -1,6 +1,9 @@
+// --> VARIÁVEIS
+
+
 // console.log("Hello World");
 
-var myName = 'Matheus';
+/* var myName = 'Matheus';
 
 let language = 'JavaScript';
 
@@ -10,4 +13,86 @@ const pattern = 'ECMAScript';
     var age = 24
 }
 
-console.log(age);
+console.log(age); 
+
+const myLastName = 'Mereb';
+
+console.log(myLastName); */
+
+
+//  -->  TIPOS DE DADOS
+
+/* var myName = "Matheus"; //string
+var age = 24; //number(int)
+var height = 1.87; //number(float)
+null
+undefined
+var isStudent = false; //boolean
+
+// Object, array and function
+
+var person = {
+    "name": "Matheus",
+    "age": 24
+}
+
+var students = [ "Maria", "João", "André"];
+
+function sum(a,b) {
+    return a + b
+} 
+
+// operador typeof
+
+function sum(){
+    return 5 + 2
+}
+
+console.log(typeof sum)
+
+*/
+
+
+// --> OPERADORES
+
+// ARITMÉTICOS
+// adição -> +
+var sumTwoNumbers = 1 + 3;
+console.log(sumTwoNumbers);
+
+// subtração -> -
+var subTwoNumbers = 4 - 3;
+console.log(subTwoNumbers);
+
+// multiplicação -> *
+var multiTwoNumbers = 4 * 3;
+console.log(multiTwoNumbers);
+
+// divisão -> /
+var divTwoNumbers = 4 / 2;
+console.log(divTwoNumbers);
+
+// módulo (resto) -> %
+console.log(8 % 3);
+
+// incrementação -> ++
+var num = 1;
+console.log(++num);
+
+// decrementação -> --
+var num = 2;
+console.log(--num);
+
+// ATRIBUIÇÃO
+
+// atribuição -> =
+
+// atribuição de soma -> +=
+
+// atribuição de subtração -> -=
+
+// atribuição de multiplicação -> *=
+
+// atribuição de divisão -> /=
+
+// atribuição de módulo -> %=
