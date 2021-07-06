06e97ee67f627e985c8b9e9395593e2d6b84a7ae
Matheus Mereb <matheus.mereb@gmail.com>
2021-07-06 15:33:03 -0300

    Finished module 04

--- 04-javascript/script.js
+++ 04-javascript/script.js
@@ -291,3 +291,40 @@ let resultado = reverseAString('Hello Gama Academy'); */
 
 let resultado = converter(40);
 console.log(`O valor em Fahrenheit é ${resultado}F`); */
+
+// 4 - To Do List:
+const listContainer = document.querySelector('[data-lists]');
+const newListForm = document.querySelector('[data-new-list-form]');
+const newListInput = document.querySelector('[data-new-list-input]');
+
+let lists = [];
+
+newListForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const listName = newListInput.value
+    if (listName === null || listName === '') return
+    const list = createList(listName)
+    newListInput.value = null
+    lists.push(list)
+    render()
+})
+
+function createList(name) {
+    return {id: Date.now().toString(), name: name}
+}
+
+function render() {
+    clearElement(listContainer)
+    lists.forEach(function (list) {
+        const item = document.createElement('li')
+        item.classList.add('item')
+        item.innerText = list.name
+        listContainer.appendChild(item)
+    })
+}
+
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
